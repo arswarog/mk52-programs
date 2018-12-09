899b6e875a2c4bb3918099323247a4fab76a87e1
Лубяной Евгений <arswarog@users.noreply.github.com>
2018-12-10 00:36:42 +0300

    Create planting-on-moon.mk

---
+++ games/planting-on-moon.mk
@@ -0,0 +1,12 @@
+{
+  "title": "Посадка на Луну",
+  "authors": [
+    {
+      "author": "Ярослав Садовский",
+    }, {
+      "author": "Arswarog",
+      "email": "arswarog@yandex.ru"
+    }
+  ],
+  "url": "http://www.lordbss.pp.ru/pmk38.html"
+}
